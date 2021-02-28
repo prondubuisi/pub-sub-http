999d7ed63cf5f67ca0e868aafd4120ec942af030
prondubuisi <onyemenamndu@gmail.com>
2021-02-28 21:20:12 +0100

    Add test seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Subscription;
+use App\Models\Publication;
+use App\Models\PublicationSubscription;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,78 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $subscription = Subscription::find(1);
+        //seed if table is empty
+        if(!$subscription)
+        {
+            $firstSubscription = Subscription::create(
+                [
+                    'topic' => 'ordershipped', 
+                    'callback_url' => 'http://localhost:8000/order1',
+                ]);
+            $secondSubscription = Subscription::create(
+                [
+                    'topic' => 'ordershipped',
+                    'callback_url' => 'http://localhost:8000/order2',
+                ]
+            );
+            $thirdSubscription = Subscription::create(
+                [
+                    'topic' => 'orderdelivered',
+                    'callback_url' => 'http://localhost:8000/order3',
+                ]
+            );
+    
+            $firstPublication = Publication::create(
+                [
+                    'topic' => 'ordershipped', 
+                    'message' => 'First order shipped',
+                ]);
+            $secondPublication = Publication::create(
+                [
+                    'topic' => 'ordershipped',
+                    'message' => 'second order shipped',
+                ]
+            );
+            $thirdPublication = Publication::create(
+                [
+                    'topic' => 'orderdelivered',
+                    'message' => 'first order delivered',
+                ]
+            );
+    
+            $firstSubscription->publications()->attach($firstPublication,
+                [
+                    'topic' => $firstPublication->topic,
+                    'message' => $firstPublication->message,
+                    'message_received' => true,
+                ]
+            );
+    
+            $secondSubscription->publications()->attach($firstPublication,
+                [
+                    'topic' => $firstPublication->topic,
+                    'message' => $firstPublication->message,
+                    'message_received' => true,
+                ]
+            );
+    
+            $secondSubscription->publications()->attach($secondPublication,
+                [
+                    'topic' => $secondPublication->topic,
+                    'message' => $secondPublication->message,
+                    'message_received' => true,
+                ]
+            );
+    
+            $thirdSubscription->publications()->attach($thirdPublication,
+                [
+                    'topic' => $thirdPublication->topic,
+                    'message' => $thirdPublication->message,
+                    'message_received' => true,
+                ]
+            );
+        }
+         
     }
 }
